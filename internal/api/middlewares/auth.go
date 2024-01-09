6a390768820022e1bb4a95cb5eed476a3e164f81
danilbushkov <danil1357997@gmail.com>
2024-01-09 15:53:32 +0300

    change responses when checking auth

--- internal/api/middlewares/auth.go
+++ internal/api/middlewares/auth.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"strings"
 
-	e "github.com/botscubes/bot-service/internal/api/errors"
-	resp "github.com/botscubes/bot-service/pkg/api_response"
-
 	"github.com/botscubes/user-service/pkg/jwt"
 	"github.com/botscubes/user-service/pkg/token_storage"
 	"github.com/gofiber/fiber/v2"
@@ -20,24 +17,24 @@ func Auth(st *token_storage.TokenStorage, jwtKey *string, log *zap.SugaredLogger
 		auth := ctx.Get("Authorization")
 
 		if !strings.HasPrefix(auth, prefix) {
-			return ctx.Status(fiber.StatusUnauthorized).JSON(resp.New(false, nil, e.ErrUnauthorized))
+			return ctx.SendStatus(fiber.StatusUnauthorized)
 		}
 
 		token := strings.TrimPrefix(auth, prefix)
 		exists, err := (*st).CheckToken(context.Background(), token)
 		if err != nil {
 			log.Errorw("failed check JWT exists (auth)", "errror", err)
-			return ctx.Status(fiber.StatusInternalServerError).JSON(resp.New(false, nil, e.ErrInternalServer))
+			return ctx.SendStatus(fiber.StatusInternalServerError)
 		}
 
 		if !exists {
-			return ctx.Status(fiber.StatusUnauthorized).JSON(resp.New(false, nil, e.ErrUnauthorized))
+			return ctx.SendStatus(fiber.StatusUnauthorized)
 		}
 
 		// WARN: fix error !!!
 		userId, err := jwt.GetIdFromToken(token, *jwtKey)
 		if err != nil {
-			return ctx.Status(fiber.StatusUnauthorized).JSON(resp.New(false, nil, e.ErrUnauthorized))
+			return ctx.SendStatus(fiber.StatusUnauthorized)
 		}
 
 		ctx.Locals("userId", int64(userId))
