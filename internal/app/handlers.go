f95a17f0710f9fc8b6b7f32c1d61f88964259667
danilbushkov <danil1357997@gmail.com>
2024-05-15 15:35:42 +0300

    change bot doc

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -27,11 +27,9 @@ func (app *App) regiterHandlers(h *handlers.ApiHandler) {
 	regBotsHandlers(bots, h)
 	regBotHandlers(bot, h)
 	regGroupHandlers(group, h)
-	//components := app.server.Group("/api/bots/:botId<int>/groups/:groupId<int>/components")
 	regComponentsHandlers(components, h)
 
 	regComponentHandlers(component, h)
-	//app.regCommandsHandlers(h)
 
 	// custom 404 handler
 	app.server.Use(handlers.NotFoundHandler)
@@ -44,8 +42,6 @@ func regBotsHandlers(bots fiber.Router, h *handlers.ApiHandler) {
 	// Get user bots
 	bots.Get("", h.GetBots)
 
-	// Wipe bot data
-	//app.server.Patch("/api/bots/:botId<int>/wipe", h.WipeBot)
 }
 
 func regBotHandlers(bot fiber.Router, h *handlers.ApiHandler) {
@@ -79,27 +75,11 @@ func regGroupHandlers(group fiber.Router, h *handlers.ApiHandler) {
 }
 
 func regComponentsHandlers(components fiber.Router, h *handlers.ApiHandler) {
-	// // Adds a component to the bot structure
-	// app.server.Post("/api/bots/:botId<int>/components", h.AddComponent)
-	//
-	// // Delete bot component
-	// app.server.Delete("/api/bots/:botId<int>/components/:compId<int>", h.DelComponent)
-	//
-	// // Delete set of components
-	// app.server.Post("/api/bots/:botId<int>/components/del", h.DelSetOfComponents)
-	//
-	// // update component
-	// app.server.Patch("/api/bots/:botId<int>/components/:compId<int>", h.UpdComponent)
-	//
-	// // Set next step component
-	// app.server.Post("/api/bots/:botId<int>/components/:compId<int>/next", h.SetNextStepComponent)
-	//
+
 	// Get bot components
 	components.Get("", h.GetBotComponents)
 	components.Post("", h.AddComponent)
 	//
-	// // Delete next step component
-	// app.server.Delete("/api/bots/:botId<int>/components/:compId<int>/next", h.DelNextStepComponent)
 }
 
 func regComponentHandlers(component fiber.Router, h *handlers.ApiHandler) {
@@ -107,20 +87,3 @@ func regComponentHandlers(component fiber.Router, h *handlers.ApiHandler) {
 	component.Patch("/position", h.SetComponentPosition)
 	component.Patch("/data", h.UpdateComponentData)
 }
-
-//func (app *App) regCommandsHandlers(h *handlers.ApiHandler) {
-//	// Add command
-//	app.server.Post("/api/bots/:botId<int>/components/:compId<int>/commands", h.AddCommand)
-//
-//	// Delete command
-//	app.server.Delete("/api/bots/:botId<int>/components/:compId<int>/commands/:commandId<int>", h.DelCommand)
-//
-//	// update command
-//	app.server.Patch("/api/bots/:botId<int>/components/:compId<int>/commands/:commandId<int>", h.UpdCommand)
-//
-//	// Set next step command
-//	app.server.Post("/api/bots/:botId<int>/components/:compId<int>/commands/:commandId<int>/next", h.SetNextStepCommand)
-//
-//	// Delete next step command
-//	app.server.Delete("/api/bots/:botId<int>/components/:compId<int>/commands/:commandId<int>/next", h.DelNextStepCommand)
-//}
